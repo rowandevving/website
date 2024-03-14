52152988455fa1c681e3dcba647f904dd297c963
Rowan <104677625+rowandevving@users.noreply.github.com>
2024-03-14 18:05:41 +0000

    add personal links to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
-export default function Header() {
+export default function Header(props: any) {
 
     return (
       <>
-        <div className="lg:h-20 h-30 bg-base flex items-center text-text">
-          <h1 className="text-3xl m-10 lg:w-full w-1/2 font-bold">
+        <div className="lg:h-20 h-30 bg-base flex items-center text-text p-10">
+          <h1 className="text-4xl lg:w-full w-1/2 font-bold mt--2">
             rowan
           </h1>
+          <div className="ml-auto flex flex-row">
+          {props.links.map((link:any, index:number) => (
+            <a href={link.url} className="visited:text-inherit text-inherit">
+                <div className={link.icon + " text-4xl hover:text-dull transition-colors duration-500 lg:ml-8 ml-6"} key={index}/>
+            </a>
+          ))}
+          </div>
         </div>
       </>
     )
