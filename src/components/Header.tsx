fc4c5143f1589b4d2d933dfc74a04df5eb81a69b
Rowan <104677625+rowandevving@users.noreply.github.com>
2024-03-14 20:29:58 +0000

    add cloudlfare analytics

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,14 @@ export default function Header(props: any) {
 
     return (
       <>
-        <div className="lg:h-20 h-30 bg-base flex items-center text-text p-10">
-          <h1 className="text-4xl lg:w-full w-1/2 font-bold mt--2">
+        <div class="lg:h-20 h-30 bg-base flex items-center text-text p-10">
+          <h1 class="text-4xl lg:w-full w-1/2 font-bold mt--2">
             rowan
           </h1>
-          <div className="ml-auto flex flex-row">
+          <div class="ml-auto flex flex-row">
           {props.links.map((link:any, index:number) => (
-            <a href={link.url} className="visited:text-inherit text-inherit">
-                <div className={link.icon + " text-4xl hover:text-dull transition-colors duration-500 lg:ml-8 ml-6"} key={index}/>
+            <a href={link.url} class="visited:text-inherit text-inherit">
+                <div class={link.icon + " text-4xl hover:text-dull transition-colors duration-500 lg:ml-8 ml-6"} key={index}/>
             </a>
           ))}
           </div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,15 +2,15 @@ export default function ProjectCard(props: any) {
 
     return (
       <>
-        <div className="bg-secondary p-5 w-full hover:scale-103 rounded-xl lg:w-1/4 transition-transform duration-500 ease-out text-text mr-10 mb-10 flex flex-col">
-          <h1 className="text-3xl font-bold m-0 md:w-full">{props.name}</h1>
-          <p className="text-dull mt-2 mb-3">
+        <div class="bg-secondary p-5 w-full hover:scale-103 rounded-xl lg:w-1/4 transition-transform duration-500 ease-out text-text mr-10 mb-10 flex flex-col">
+          <h1 class="text-3xl font-bold m-0 md:w-full">{props.name}</h1>
+          <p class="text-dull mt-2 mb-3">
             {props.description}
           </p>
-          <div className="m-0 flex items-center mt-auto">
+          <div class="m-0 flex items-center mt-auto">
           {props.links.map((link:any, index:number) => (
-            <a href={link.url} className="visited:text-inherit text-inherit">
-              <div className={link.icon + " text-3xl hover:text-dull transition-colors duration-500 mr-3"} key={index}/>
+            <a href={link.url} class="visited:text-inherit text-inherit">
+              <div class={link.icon + " text-3xl hover:text-dull transition-colors duration-500 mr-3"} key={index}/>
             </a>
           ))}
           </div>
